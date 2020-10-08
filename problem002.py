715b7cb2b6ef83f5bebe86ed47146248087afb20
RoopdeepKaur <72173932+RoopdeepKaur@users.noreply.github.com>
2020-10-08 15:30:38 +1100

    Create problem002.py

---
+++ problem002.py
@@ -0,0 +1,21 @@
+def fib():
+    x, y = 0, 1
+    while True:
+        yield x
+        x, y = y, x + y
+
+
+def even(seq):
+    for number in seq:
+        if not number % 2:
+            yield number
+
+
+def four_million(seq):
+    for number in seq:
+        if number > 4000000:
+            break
+        yield number
+
+
+print(sum(even(four_million(fib()))))
