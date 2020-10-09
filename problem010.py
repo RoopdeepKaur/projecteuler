032c94ba1cbe91db4ff99cbd7369dff14b0bdbd1
RoopdeepKaur <72173932+RoopdeepKaur@users.noreply.github.com>
2020-10-09 13:41:49 +1100

    problem010.py
    The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
    
    Find the sum of all the primes below two million.

---
+++ problem010.py
@@ -0,0 +1,18 @@
+def check_prime(num):
+    if num > 2 and num % 2 == 0:
+        return False
+    else:
+        for i in range(3, int(math.sqrt(num)) + 1, 2):
+            if num % i == 0:
+                return False
+    return True
+
+
+def find_sum(limit):
+    sum = 0
+    for i in range(2, limit):
+        if check_prime(i):
+            sum += i
+    return sum
+if __name__ == '__main__':
+    print(f"The sum of prime numbers under two million is {find_sum(2000000)}")
